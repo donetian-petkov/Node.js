008d10101600b3ea6a8d734a6a8cde7c5660f529
donetian-petkov <donetian+petkov@gmail.com>
2022-06-19 16:35:06 +0300

    Render Static Page

--- 3-workshop/src/controllers/homeController.js
+++ 3-workshop/src/controllers/homeController.js
@@ -1,9 +1,24 @@
 const cubes = require('../db.json');
+const router = require('express').Router();
 
-exports.index = (req, res) => {
+/*exports.index = (req, res) => {
     res.render('index', { cubes });
 }
 
 exports.about = (req, res) => {
     res.render('about');
-}
+}*/
+
+router.get('/', (req, res) => {
+    res.render('index', { cubes });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+/*router.post('/submittedForm', (req, res) => {
+    res.send('Successful form submission!');
+})*/
+
+module.exports = router;
--- 3-workshop/src/routes.js
+++ 3-workshop/src/routes.js
@@ -4,8 +4,10 @@ const cubeController = require('./controllers/cubeController');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', homeController.index);
-router.get('/about', homeController.about);
+/*router.get('/', homeController.index);
+router.get('/about', homeController.about);*/
+
+router.use('/', homeController);
 router.use('/cube', cubeController);
 
 
