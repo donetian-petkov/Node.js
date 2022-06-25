5fa5b2bdd032eb15cf0a4fcee88b39fd894f11f1
donetian-petkov <donetian+petkov@gmail.com>
2022-06-25 08:53:21 +0300

    Share Functionality

--- exam-preparation/controllers/publicationController.js
+++ exam-preparation/controllers/publicationController.js
@@ -3,6 +3,7 @@ const { isAuth } = require('../middlewares/authMiddleware');
 const publicationService = require('../services/publicationService');
 const {errorMapper} = require("../utils/errorMapper");
 const { preloadPublication, isAuthor } = require('../middlewares/publicationMiddlewares')
+const mongoose = require("mongoose");
 
 router.get('/', async (req,res) => {
 
@@ -16,12 +17,15 @@ router.get('/:publicationId/details', async (req,res) => {
 
     const publication = await publicationService.getOneDetailed(req.params.publicationId).lean();
     const isAuthor = publication.author._id == req.user?._id;
+    const isShared = publication.usersShared.join(',').includes(req.user._id);
 
-    res.render('publication/details', { ...publication, isAuthor });
+    res.render('publication/details', { ...publication, isAuthor, isShared });
 
 });
 
 router.get('/:publicationId/edit', isAuth, preloadPublication, isAuthor, async (req,res, next) => {
+
+    console.log(req.publication);
 
     res.render('publication/edit', { ...req.publication });
 
@@ -43,10 +47,22 @@ router.post('/:publicationId/delete', isAuth, preloadPublication, isAuthor, asyn
 
     await publicationService.delete(req.params.publicationId);
 
-    res.render('publication/edit')
+    res.render('publication');
 
 
-})
+});
+
+router.get('/:publicationId/share', isAuth, async (req, res) => {
+
+    const publication = await publicationService.getOne(req.params.publicationId);
+
+    publication.usersShared.push(req.user._id);
+
+    await publication.save();
+
+    res.redirect('/');
+
+});
 
 router.get('/create', isAuth,(req, res) => {
     res.render('publication/create')
@@ -62,7 +78,7 @@ router.post('/create', isAuth, async (req, res) => {
         res.render('publication/create', {...req.body, error: errorMapper(error)});
     }
 
-})
+});
 
 
 module.exports = router;
--- exam-preparation/middlewares/publicationMiddlewares.js
+++ exam-preparation/middlewares/publicationMiddlewares.js
@@ -4,14 +4,14 @@ exports.preloadPublication = async (req, res, next) => {
 
     const publication = await publicationService.getOne(req.params.publicationId).lean();
 
-    res.publication = publication;
+    req.publication = publication;
 
     next();
 }
 
 exports.isAuthor = (req, res, next) => {
 
-    if (publication.author != req.user._id) {
+    if (req.publication.author != req.user._id) {
 
         return next({message: 'You are not authorised!', status: 401});
 
